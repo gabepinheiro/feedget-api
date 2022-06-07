ea5ad926100b8d581068490a27b15b712d46e5d1
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-07 14:41:34 -0300

    feat: create route post feedbacks and create feedback on db with prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import { prismaClient } from './prisma'
 
 const app = express()
 
-app.get('/users', (req, res) => {
-  return res.send('Hello World')
+app.use(express.json())
+
+app.post('/feedbacks', async (req, res) => {
+  const feedback = await prismaClient.feedback.create({
+    data: req.body
+  })
+
+  return res.status(201).json({ data: feedback })
 })
 
 app.listen(3333, () => console.log('HTTP server is running on port 3333'))
